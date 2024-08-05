a243ad3d42f49eb84704032ab7f640f9d50089d9
Nishy31 <146456083+Nishy31@users.noreply.github.com>
2024-08-05 13:18:24 -0300

    Added dancing man emoji and changed text and background colour.

--- GitHubTest/ContentView.swift
+++ GitHubTest/ContentView.swift
@@ -10,7 +10,15 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
+            
+            Image(systemName: "figure.dance")
+                .font(.system(size: 80))
+                .foregroundStyle(.cyan)
             Text("I am getting somewhere!!!")
+                .font(.title)
+                .foregroundStyle(.cyan)
+            
+            
         }
         .padding()
     }
