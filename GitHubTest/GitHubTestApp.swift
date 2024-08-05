02e01f3310475c7079780dec23ed9ab5d3c0447d
Nishy31 <146456083+Nishy31@users.noreply.github.com>
2024-08-05 13:04:26 -0300

    Initial Commit

---
+++ GitHubTest/GitHubTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitHubTestApp.swift
+//  GitHubTest
+//
+//  Created by Nishta Gokul on 2024-08-05.
+//
+
+import SwiftUI
+
+@main
+struct GitHubTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
